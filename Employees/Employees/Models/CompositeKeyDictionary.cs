7d2b1d2b4e65d136191d89a20a9c63e8121351a2
Georgi Dimitrov <Themikuma@gmail.com>
2024-01-14 15:56:45 +0200

    Fixed an error in model placement.

--- Employees/Employees/Models/CompositeKeyDictionary.cs
+++ Employees/Employees/Models/CompositeKeyDictionary.cs
@@ -1,9 +1,64 @@
 ﻿namespace Employees.Models
 {
-    internal class CompositeKeyDictionary
+
+    public class CompositeKeyDictionary
     {
+        private readonly Dictionary<string, int> _calculatedDurations;
         public CompositeKeyDictionary()
         {
+            _calculatedDurations = new Dictionary<string, int>();
+        }
+
+        public void Add(EmploymentDuration duration1, EmploymentDuration duration2)
+        {
+            List<long> values =
+            [
+                duration1.DateTo - duration1.DateFrom,//endA - startA
+                duration1.DateTo - duration2.DateFrom,//endA - startB
+                duration2.DateTo - duration1.DateFrom,//endB - startA
+                duration2.DateTo - duration2.DateFrom,//endB - startB
+            ];
+            long overlap = values.Min();
+
+            if (overlap >= 0)
+            {
+                string key = CreateKey(duration1.EmpId, duration2.EmpId);
+                if (_calculatedDurations.ContainsKey(key))
+                {
+                    _calculatedDurations[key] += (int)(overlap / Constants.SecondsInDay);
+                }
+                else
+                {
+                    _calculatedDurations.Add(key, (int)(overlap / Constants.SecondsInDay));
+                }
+            }
+        }
+
+        public string GetKey(EmploymentDuration duration1, EmploymentDuration duration2)
+        {
+            return CreateKey(duration1.EmpId, duration2.EmpId);
+        }
+
+        public int GetDuration(int EmpId1, int EmpId2)
+        {
+            var key = CreateKey(EmpId1, EmpId2);
+            return _calculatedDurations[key];
+        }
+
+        public int GetDuration(string key) => _calculatedDurations[key];
+
+        private string CreateKey(int employeeId1, int employeeId2)
+        {
+            string key;
+            if (employeeId1 > employeeId2)
+            {
+                key = $"{employeeId2},{employeeId1}";
+            }
+            else
+            {
+                key = $"{employeeId1},{employeeId2}";
+            }
+            return key;
         }
     }
 }
